b4d6a00ec625e681e080c3fe80177eac5c850b82
Pradum Kumar <pradumky803@gmail.com>
2026-04-21 23:51:02 +0530

    fix: add osmdroid and Coil libraries

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -67,4 +67,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.osmdroid.android)
+    implementation(libs.coil.compose)
 }
